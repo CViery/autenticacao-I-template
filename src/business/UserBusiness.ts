570452812588db3b82c35bee7fb975e6048fc85c
cristian-viery <viery.guimaraes01@gmail.com>
2023-08-23 16:26:09 -0300

    pronto

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -5,16 +5,29 @@ import { SignupInputDTO, SignupOutputDTO } from "../dtos/user/signup.dto"
 import { BadRequestError } from "../errors/BadRequestError"
 import { NotFoundError } from "../errors/NotFoundError"
 import { USER_ROLES, User } from "../models/User"
+import { HashManager } from '../services/HashManeger'
+import { IdGenerator } from '../services/IdGenerator'
+import { TokenMenanger, TokenPayload } from '../services/TokenMenager'
 
 export class UserBusiness {
   constructor(
-    private userDatabase: UserDatabase
+    private userDatabase: UserDatabase,
+    private idGenerator: IdGenerator,
+    private TokenMenager: TokenMenanger,
+    private HashManager : HashManager
+
   ) { }
 
   public getUsers = async (
     input: GetUsersInputDTO
   ): Promise<GetUsersOutputDTO> => {
-    const { q } = input
+    const { q, token } = input
+    const payload = this.TokenMenager.getPaylod(token)
+    
+    if(payload?.role !== USER_ROLES.ADMIN){
+      throw new BadRequestError("Só Admim pode acessar");
+      
+    }
 
     const usersDB = await this.userDatabase.findUsers(q)
 
@@ -39,19 +52,22 @@ export class UserBusiness {
   public signup = async (
     input: SignupInputDTO
   ): Promise<SignupOutputDTO> => {
-    const { id, name, email, password } = input
+    const { name, email, password } = input
 
-    const userDBExists = await this.userDatabase.findUserById(id)
+    const userDBExists = await this.userDatabase.findUserByEmail(email)
 
     if (userDBExists) {
-      throw new BadRequestError("'id' já existe")
+      throw new BadRequestError("'email' já Cadastrado")
     }
+    const id = this.idGenerator.generateId()
 
+    const hashedPassowrd = await this.HashManager.hash(password)
+    
     const newUser = new User(
       id,
       name,
       email,
-      password,
+      hashedPassowrd,
       USER_ROLES.NORMAL, // só é possível criar users com contas normais
       new Date().toISOString()
     )
@@ -59,9 +75,15 @@ export class UserBusiness {
     const newUserDB = newUser.toDBModel()
     await this.userDatabase.insertUser(newUserDB)
 
+    const token = this.TokenMenager.createToken({
+      id: newUser.getId(),
+      role: newUser.getRole(),
+      name: newUser.getName()
+    })
+
     const output: SignupOutputDTO = {
       message: "Cadastro realizado com sucesso",
-      token: "token"
+      token: token
     }
 
     return output
@@ -77,14 +99,28 @@ export class UserBusiness {
     if (!userDB) {
       throw new NotFoundError("'email' não encontrado")
     }
+    const isPasswordIsValid = await this.HashManager.compare(password,userDB.password)
 
-    if (password !== userDB.password) {
-      throw new BadRequestError("'email' ou 'password' incorretos")
+    if (!isPasswordIsValid) {
+      throw new BadRequestError("'password' incorreta")
     }
-
+    const user = new User{
+      userDB.id,
+      userDB.name,
+      userDB.email,
+      userDB.password,
+      userDB.role,
+      userDB.created_at
+    }
+    const payload: TokenPayload = {
+      id: userDB.id,
+      name: userDB.name,
+      role: userDB.role
+    }
+    const token = this.TokenMenager.createToken(payload)
     const output: LoginOutputDTO = {
       message: "Login realizado com sucesso",
-      token: "token"
+      token: token
     }
 
     return output
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -2,12 +2,18 @@ import express from "express"
 import { UserBusiness } from "../business/UserBusiness"
 import { UserController } from "../controller/UserController"
 import { UserDatabase } from "../database/UserDatabase"
+import { IdGenerator } from '../services/IdGenerator'
+import { TokenMenanger } from '../services/TokenMenager'
+import { HashManager } from '../services/HashManeger'
 
 export const userRouter = express.Router()
 
 const userController = new UserController(
     new UserBusiness(
-        new UserDatabase()
+        new UserDatabase(),
+        new IdGenerator(),
+        new TokenMenanger(),
+        new HashManager()
     )
 )
 
